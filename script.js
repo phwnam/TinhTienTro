c4a9587e86910c130528c71c3952fdeadb2701c4
Nguyễn Phương Nam <namiiu2209@gmail.com>
2024-04-24 11:32:01 +0700

    Ver2

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 function calculate() {
     var electricity = parseFloat(document.getElementById('electricity').value);
+    var electricityPrice = parseFloat(document.getElementById('electricity-price').value);
     var water = parseFloat(document.getElementById('water').value);
+    var waterPrice = parseFloat(document.getElementById('water-price').value);
     var internet = parseFloat(document.getElementById('internet').value);
+    var service = parseFloat(document.getElementById('service').value);
   
-    var electricityCost = electricity * 3000; // 
-    var waterCost = water * 14000; // 
+    var electricityCost = electricity * electricityPrice; // 
+    var waterCost = water * waterPrice; // 
     var internetCost = internet; // 
+    var serviceCost = service;
   
-    var totalCost = electricityCost + waterCost + internetCost;
+    var totalCost = electricityCost + waterCost + internetCost + serviceCost;
   
     var resultDiv = document.getElementById('result');
     var electricityDiv = document.getElementById('electricityCost');
